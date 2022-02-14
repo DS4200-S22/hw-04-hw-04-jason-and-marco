7943a30a75a014a64d9040ccf40b63a58219a192
Jason Zhang <jasonjzhang17@gmail.com>
2022-02-13 19:52:44 -0500

    Adding semicolons to end lines for best practice

--- main.js
+++ main.js
@@ -1,41 +1,41 @@
 function countClicks() {
-    console.log("Counting clicks")
+    console.log("Counting clicks");
 
-    let obj = document.getElementById("counter")
-    let count = obj.innerHTML
-    console.log(count)
+    let obj = document.getElementById("counter");
+    let count = obj.innerHTML;
+    console.log(count);
 
-    obj.innerHTML = parseInt(count) + 1
+    obj.innerHTML = parseInt(count) + 1;
 }
 
 function showBorder() {
-    console.log("Showing border")
+    console.log("Showing border");
 
-    let border = document.getElementById("circle2").getAttribute("stroke-width")
-    console.log(border)
+    let border = document.getElementById("circle2").getAttribute("stroke-width");
+    console.log(border);
 
-    document.getElementById("circle2").setAttribute("stroke-width", "5")
+    document.getElementById("circle2").setAttribute("stroke-width", "5");
 }
 
 function removeBorder() {
-    console.log("Removing border")
+    console.log("Removing border");
 
-    let border = document.getElementById("circle2").getAttribute("stroke-width")
-    console.log(border)
+    let border = document.getElementById("circle2").getAttribute("stroke-width");
+    console.log(border);
 
-    document.getElementById("circle2").setAttribute("stroke-width", "0")
+    document.getElementById("circle2").setAttribute("stroke-width", "0");
 }
 
 function toggleVisibility() {
-    console.log("Toggling visiblity")
+    console.log("Toggling visiblity");
 
-    let opacity = document.getElementById("circle3").getAttribute("opacity")
-    console.log(opacity)
+    let opacity = document.getElementById("circle3").getAttribute("opacity");
+    console.log(opacity);
 
     if (opacity === "100") {
-        document.getElementById("circle3").setAttribute("opacity", "0")
+        document.getElementById("circle3").setAttribute("opacity", "0");
     }
     else {
-        document.getElementById("circle3").setAttribute("opacity", "100")
+        document.getElementById("circle3").setAttribute("opacity", "100");
     }
 }
